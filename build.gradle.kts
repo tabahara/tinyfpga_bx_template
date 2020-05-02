27634195d9b8f36d62302b5312570578e6812376
Hiroki Tsukahara <asuka@quinmantha.com>
2020-05-02 14:58:40 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+open class ApioTask : Exec() {
+    init {
+        group = "apio"
+    }
+}
+
+val apioBuild = task<ApioTask>("apioBuild") {
+    commandLine("apio", "build")
+}
+
+val apioClean = task<ApioTask>("apioClean") {
+    commandLine("apio", "clean")
+}
+
+val apioUpload = task<ApioTask>("apioUpload") {
+    commandLine("apio", "upload")
+}
+
+apioUpload.dependsOn(apioBuild)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# setup project
+
+1. graldew の配置
+% gradle wrapper
+
+2. gradleの版の設定
+必要に応じてgradle properties の修正する。
+
+
